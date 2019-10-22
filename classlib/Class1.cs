9bbcd868903651bb5efdf03498fb27af565fb292
ray.hayes <ray.hayes@outlook.com>
2019-10-22 10:09:25 +0100

    Initial working Core3 + .NetStandard

---
+++ classlib/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLib
+{
+    using OfficeOpenXml;
+
+    public class Class1
+    {
+        public void MakeExcel()
+        {
+            using (var package = new ExcelPackage())
+            {
+                // Use 'package'
+                package.Compatibility.IsWorksheets1Based = true;
+            }
+        }
+    }
+}
